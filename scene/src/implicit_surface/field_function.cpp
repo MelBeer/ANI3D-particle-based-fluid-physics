5c88c2f306192f3c0fd3ed262019734c98914547
melvin.gidel <melvin.gidel@epita.fr>
2022-10-16 12:24:20 +0200

    optimized field function operator; fixed simulate clipping effect

--- scene/src/implicit_surface/field_function.cpp
+++ scene/src/implicit_surface/field_function.cpp
@@ -1,5 +1,7 @@
 #include "field_function.hpp"
 
+#define EPSILON 0.001f
+
 using namespace cgp;
 
 // Parameterization Gaussian centered at point p0
@@ -13,10 +15,32 @@ static float gaussian(vec3 const& p, vec3 const& p0, float sigma)
 float field_function_structure::operator()(cgp::vec3 const& p) const
 {
 	float value = 0.0f;
-	for (size_t i = 0; i < particles.size(); i++)
-	{
-		particle_structure pa = particles[i];
-		value += pa.m * gaussian(p, pa.p, pa.r);
+	
+	int kx = (p.x + 1 - EPSILON) * hash_grid.dimension.x / 2;
+	int ky = (p.y + 1 - EPSILON) * hash_grid.dimension.y / 2;
+	int kz = (p.z + 1 - EPSILON) * hash_grid.dimension.z / 2;
+
+	// For each neighbouring cell
+	for (int off_x = -1; off_x < 2; off_x++) {
+		for (int off_y = -1; off_y < 2; off_y++) {
+			for (int off_z = -1; off_z < 2; off_z++) {
+
+				if (kx + off_x < 0 || kx + off_x > hash_grid.dimension.x - 1
+					|| ky + off_y < 0 || ky + off_y > hash_grid.dimension.y - 1
+					|| kz + off_z < 0 || kz + off_z > hash_grid.dimension.z - 1)
+					continue;
+								
+				std::vector<int> const &neighbour_particles_indices = hash_grid(kx + off_x, ky + off_y, kz + off_z);
+
+				for (size_t pi = 0; pi < neighbour_particles_indices.size(); pi++)
+				{
+					size_t i = neighbour_particles_indices[pi];
+
+					particle_structure pa = particles[i];
+					value += pa.m * gaussian(p, pa.p, pa.r);
+				}
+			}
+		}
 	}
 
 	if (noise_magnitude > 0) {
--- scene/src/simulation/simulation.cpp
+++ scene/src/simulation/simulation.cpp
@@ -14,7 +14,7 @@ int3 grid_indices_from_pos(grid_3D<T> grid, vec3 pos) {
 	int kz = (pos.z + 1 - EPSILON) * grid.dimension.z / 2;
 
 	if (kx < 0 || ky < 0 || kz < 0)
-		std::cout << "HEEELP !! pos = " << pos << std::endl;
+		std::cout << "HEeELP !! pos = " << pos << std::endl;
 
 	return { kx, ky, kz };
 };
@@ -30,15 +30,12 @@ std::vector<int3> simulate(std::vector<particle_structure>& particles,
 
 	std::vector<bool> seen(particles.size(), false);
 	
+	// For each cell
 	for (size_t kx = 0; kx < hash_grid.dimension.x; kx++) {
 		for (size_t ky = 0; ky < hash_grid.dimension.y; ky++) {
 			for (size_t kz = 0; kz < hash_grid.dimension.z; kz++) {
-				
-				std::vector<int> &particles_indices = hash_grid(kx, ky, kz);
 
-				/*
-					TODO : check for surrounding cells as well
-				*/
+				std::vector<int> &particles_indices = hash_grid(kx, ky, kz);
 
 				// For each particle in this cell kx,ky,kz
 				for (size_t pi = 0; pi < particles_indices.size(); pi++)
@@ -49,53 +46,60 @@ std::vector<int3> simulate(std::vector<particle_structure>& particles,
 					seen[k] = true;
 					
 					particle_structure& part = particles[k];
-					vec3 savep = part.p;
 
 					vec3 const f = part.m * g;
 
-					// For every other Particle (all but k)
-					for (size_t pj = 0; pj < particles_indices.size(); pj++)
-					{
-						int j = particles_indices[pj];
-						if (j == k) continue;
+					// For each neighbouring cell
+					for (int off_x = -1; off_x < 2; off_x++) {
+						for (int off_y = -1; off_y < 2; off_y++) {
+							for (int off_z = -1; off_z < 2; off_z++) {
 
-						particle_structure& part_1 = particles[j];
-						if (part.p.x == part_1.p.x && part.p.y == part_1.p.y && part.p.z == part_1.p.z)
-							part_1.p += 0.000001f;
-						vec3 savep1 = part_1.p;
-						// If they collide
-						if (norm(part.p - part_1.p) <= (part.r + part_1.r) + EPSILON)
-						{
-							vec3 u = (part.p - part_1.p) / norm(part.p - part_1.p);
+								if (kx + off_x < 0 || kx + off_x > hash_grid.dimension.x - 1
+									|| ky + off_y < 0 || ky + off_y > hash_grid.dimension.y - 1
+									|| kz + off_z < 0 || kz + off_z > hash_grid.dimension.z - 1)
+									continue;
+												
+								std::vector<int> &neighbour_particles_indices = hash_grid(kx + off_x, ky + off_y, kz + off_z);
 
-							vec3 v_ortho = dot(part.v, u) * u;
-							vec3 v_par = part.v - v_ortho;
-							vec3 v1_ortho = dot(part_1.v, u) * u;
-							vec3 v1_par = part_1.v - v1_ortho;
+								// For every other Particle (all but k) in surrounding cells
+								for (size_t pj = 0; pj < neighbour_particles_indices.size(); pj++)
+								{
+									int j = neighbour_particles_indices[pj];
+									if (j == k) continue;
 
-							if (norm(part.v) > EPSILON)
-								part.v = ALPHA * v_par + BETA * v1_ortho; // += dot(part_1.v - part.v, u) * u;
-							else
-								part.v = MU * part.v;
+									particle_structure& part_1 = particles[j];
+									if (part.p.x == part_1.p.x && part.p.y == part_1.p.y && part.p.z == part_1.p.z)
+										part_1.p += 0.000001f;
 
-							if (norm(part_1.v) > EPSILON)
-								part_1.v = ALPHA * v1_par + BETA * v_ortho; // -= dot(part_1.v - part.v, u) * u;
-							else
-								part_1.v = MU * part_1.v;
+									// If they collide
+									if (norm(part.p - part_1.p) <= (part.r + part_1.r) + EPSILON)
+									{
+										vec3 u = (part.p - part_1.p) / norm(part.p - part_1.p);
 
-							float d = part.r + part_1.r + EPSILON - norm(part.p - part_1.p);
-							part.p += d/2 * u;
-							part_1.p -= d/2 * u;
+										vec3 v_ortho = dot(part.v, u) * u;
+										vec3 v_par = part.v - v_ortho;
+										vec3 v1_ortho = dot(part_1.v, u) * u;
+										vec3 v1_par = part_1.v - v1_ortho;
 
-							if (part.p.x != part.p.x) 
-								std::cout << "savep = " << savep << " | savep1 = " << savep1 << " | " << __LINE__ << std::endl;
+										if (norm(part.v) > EPSILON)
+											part.v = ALPHA * v_par + BETA * v1_ortho; // += dot(part_1.v - part.v, u) * u;
+										else
+											part.v = MU * part.v;
+
+										if (norm(part_1.v) > EPSILON)
+											part_1.v = ALPHA * v1_par + BETA * v_ortho; // -= dot(part_1.v - part.v, u) * u;
+										else
+											part_1.v = MU * part_1.v;
+
+										float d = part.r + part_1.r + EPSILON - norm(part.p - part_1.p);
+										part.p += d/2 * u;
+										part_1.p -= d/2 * u;
+									}
+								}
+							}
 						}
 					}
-
-					if (part.p.x != part.p.x) 
-						std::cout << "savep = " << savep << " | " << __LINE__ << std::endl;
 					
-
 					for (size_t i = 0; i < faces.size(); i++)
 					{
 						double dist = dot(part.p - faces[i], normals[i]);
@@ -106,20 +110,19 @@ std::vector<int3> simulate(std::vector<particle_structure>& particles,
 						}
 					}
 
-					if (part.p.x != part.p.x) 
-						std::cout << "savep = " << savep << " | " << __LINE__ << std::endl;
-
 					part.v = (1 - 0.9f * dt) * part.v + dt * f;
 					part.p = part.p + dt * part.v;
-
-					if (part.p.x != part.p.x) 
-						std::cout << "savep = " << savep << " | " << __LINE__ << std::endl;
 
 					int3 grid_i = grid_indices_from_pos<>(hash_grid, part.p);
 
 					if (grid_i.x != kx || grid_i.y != ky || grid_i.z != kz) {
 						particles_indices.erase(particles_indices.begin() + pi);
-						hash_grid(grid_i).push_back(k);
+						std::vector<int> &dest_cell = hash_grid(grid_i);
+						dest_cell.push_back(k);
+
+						if (grid_i.x <= kx && grid_i.y <= ky && grid_i.z <= kz && dest_cell.size() == 1) {
+							occupied_cells.push_back({ kx,ky,kz });
+						}
 					}
 				}
 
